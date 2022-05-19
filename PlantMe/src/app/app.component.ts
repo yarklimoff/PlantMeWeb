ff6c14faad81bee736f990a032312d64672d5d90
Klimov Yaroslav <klimov.iav@phystech.edu>
2022-05-19 10:11:16 +0300

    add auth

---
+++ PlantMe/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import {AuthenticationService} from "./services/authentication.service";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  constructor(public authService: AuthenticationService,
+              private router: Router) {
+  }
+
+  logout() {
+    this.authService.logout().subscribe(() => {
+      this.router.navigate(['']);
+    });
+  }
+
+}
---
+++ PlantMe/src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'plantme-5875a',
+    appId: '1:12645099011:web:1bd245f5c78cdd19579036',
+    storageBucket: 'plantme-5875a.appspot.com',
+    apiKey: 'AIzaSyBvVGsnHjfko5zKeaCXiUUN0ZOTmwRA6jU',
+    authDomain: 'plantme-5875a.firebaseapp.com',
+    messagingSenderId: '12645099011',
+    measurementId: 'G-ZZ0VF5T3YE',
+  },
+  production: true
+};
